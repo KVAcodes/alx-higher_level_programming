648e8740c00b8b43a5b8754470a8bfe7a350688e
KVAcodes <kadiriavictor@gmail.com>
2023-01-03 10:34:06 +0100

    fixed 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,32 +1,21 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * check_cycle - Checks if a singly linked list has a cycle
- * in it.
- * @list: The head(address of the first node).
+ * check_cycle - checks if a singly linked list has a loop.
+ * @list: The head of the linked list.
  *
  * Return: 0 if there is no cycle, 1 if there is a cycle.
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *tortoise = list;
-	listint_t *hare = list;
+	listint_t *hare = list, *tortoise = list;
 
-	if (list == NULL)
-	{
-		return (0);
-	}
-
-	while ((hare != NULL) && (hare->next != NULL))
+	while (hare && hare->next && hare->next->next)
 	{
 		tortoise = tortoise->next;
 		hare = hare->next->next;
-		if (tortoise == hare)
-		{
+		if (hare == tortoise)
 			return (1);
-		}
 	}
 	return (0);
 }
