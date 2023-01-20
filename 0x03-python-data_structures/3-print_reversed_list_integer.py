a30c5fef6d3446a13894d5e3c54b5c91f1b3d30c
KVAcodes <kadiriavictor@gmail.com>
2023-01-20 16:58:31 +0100

    fixed?

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,6 @@
 def print_reversed_list_integer(my_list=[]):
     if len(my_list) == 0:
         return
-    imy_list.reverse()
+    my_list.reverse()
     for num in my_list:
         print("{:d}".format(num))
