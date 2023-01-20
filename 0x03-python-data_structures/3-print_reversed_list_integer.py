0f949f4d4ff099b15364376fc02d09db556bfbc9
KVAcodes <kadiriavictor@gmail.com>
2023-01-20 17:01:43 +0100

    trying other fixes

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0:
-        return
-    my_list.reverse()
-    for num in my_list:
-        print("{:d}".format(num))
+    if my_list:
+        my_list.reverse()
+        for num in my_list:
+            print("{:d}".format(num))
