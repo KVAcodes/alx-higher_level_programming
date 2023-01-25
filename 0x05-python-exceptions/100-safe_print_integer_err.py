b379147529291cc54ab4c184e863b7e2d35ee1d3
KVAcodes <kadiriavictor@gmail.com>
2023-01-25 15:33:57 +0100

    added task 7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import stderr
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (TypeError, ValueError) as err:
+        print("Exception: {}".format(err), file=stderr)
+        return False
+    else:
+        return True
