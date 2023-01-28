7b8efe8e71e694fd627e98251d7a78b7a2c19d21
KVAcodes <kadiriavictor@gmail.com>
2023-01-28 12:50:29 +0100

    added task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains the definition of the Class Square.
+"""
+
+
+class Square:
+    """This is an empty Square class.
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,4 @@
+# 0x06. Python - Classes and Objects
+
+### AUTHOR :open_book:
+Kadiri Victor.
