97e6b0d8e66b51fbbe56d8b348663c62152dadfb
KVAcodes <kadiriavictor@gmail.com>
2023-01-28 14:53:45 +0100

    added task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+"""This module contains the definition of the Class Square.
+"""
+
+
+class Square:
+    """This class defines a Square object as best a possible.
+    """
+    def __init__(self, size=0):
+        """The constructor method of the Square instances.
+
+        Args:
+            size (int): The size of the Square object.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the Square object.
+
+        Args:
+            None.
+
+        Returns:
+            The area of the Square instance.
+        """
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """Prints the Square object in a graphical format using
+        the '#' character.
+
+        Args:
+            None
+        Returns:
+            None
+        """
+        if (self.size == 0):
+            print()
+            return
+        for col in range(self.size):
+            for row in range(self.size):
+                print("#", end="")
+            print()
+        return
