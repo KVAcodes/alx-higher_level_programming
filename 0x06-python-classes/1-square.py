6fb44c98e48d4673087fdecb0e4c7fcd3d9d00d6
KVAcodes <kadiriavictor@gmail.com>
2023-01-28 13:05:40 +0100

    added task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""This module contains the definition of the Class Square.
+"""
+
+
+class Square:
+    """This class defines a Square object as best a possible.
+    """
+    def __init__(self, size):
+        """The constructor method of the Square instances.
+
+        Args:
+            size (int): The size of the Square object.
+        """
+        self.__size = size
