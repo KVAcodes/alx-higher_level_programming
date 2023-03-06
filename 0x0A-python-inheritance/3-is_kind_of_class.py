d76cdf5052961866f4b6c50b1442862fad14e38f
KVAcodes <kadiriavictor@gmail.com>
2023-03-06 13:56:14 +0100

    added new stuff

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This module contains the definition of the 2-is_kind_of_class.py function.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    return True if isinstance(obj, a_class) else False
