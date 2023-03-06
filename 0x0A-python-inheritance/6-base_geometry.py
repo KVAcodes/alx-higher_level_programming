2618212849aa4330ad82893806ea2b0af8c29b15
KVAcodes <kadiriavictor@gmail.com>
2023-03-06 14:21:34 +0100

    added more stuff

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This module contains the definition of class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """ class BaseGeometry.
+    """
+    def area(self):
+        """raise an Exception.
+        """
+        raise Exception("area() is not implemented")
