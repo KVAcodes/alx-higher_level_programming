df3ea68bb5facb6b667f98ec910d77717a6b8c7c
KVAcodes <kadiriavictor@gmail.com>
2023-03-06 14:12:02 +0100

    added new

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-""" This module contains the definition of the 2-is_kind_of_class.py function.
+""" This module contains the definition of the inherits_from function.
 """
 
 
-def is_kind_of_class(obj, a_class):
-    return True if isinstance(obj, a_class) else False
+def inherits_from(obj, a_class):
+    """ returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise False.
+    """
+    return issubclass(type(obj), a_class) if not type(obj) == a_class else \
+    False
