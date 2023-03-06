158e9b8f2f986d070fecc088f94984dc3d1037f6
KVAcodes <kadiriavictor@gmail.com>
2023-03-06 13:05:07 +0100

    :crossed_fingers:

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+""" This module contains the lookup function definition.
+"""
+
 
 def lookup(obj):
+    """ return the list of available attributes and methods
+    of an object "obj".
+    """
     return dir(obj)
