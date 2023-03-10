c5e379fb1dc3c44d4426d570139122e7fc9d7006
KVAcodes <kadiriavictor@gmail.com>
2023-03-10 16:47:34 +0100

    :crosses_fingers:

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module contains the Student class defintion.
+"""
+
+
+class Student:
+    """The Student class.
+    """
+    def __init__(self, first_namme, last_name, age):
+        """Constructor of the Student instances.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """that retrieves a dictionary representation of a Student instance.
+        """
+        return self.__dict__
