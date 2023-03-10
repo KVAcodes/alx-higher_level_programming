a90a3cb45f1715e47b0e6b9598bdea4e1a2aa40e
KVAcodes <kadiriavictor@gmail.com>
2023-03-10 16:39:25 +0100

    Not tested :crossed_fingers:

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains the class_to_json function definition.
+"""
+
+
+import json
+
+
+def class_to_json(obj):
+    """ returns the dictionary description with simple data structure (list,
+    dictionary, string, integer and boolean) for JSON serialization of an
+    object.
+    """
+    returns json.dumps(obj.__dict__)
