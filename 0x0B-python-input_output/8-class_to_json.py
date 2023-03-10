9187c8f935fcb999a6f012d592c17ced2bcac150
KVAcodes <kadiriavictor@gmail.com>
2023-03-10 16:41:51 +0100

    Not tested :crossed_fingers:

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,12 +3,9 @@
 """
 
 
-import json
-
-
 def class_to_json(obj):
     """ returns the dictionary description with simple data structure (list,
     dictionary, string, integer and boolean) for JSON serialization of an
     object.
     """
-    returns json.dumps(obj.__dict__)
+    return obj.__dict__
