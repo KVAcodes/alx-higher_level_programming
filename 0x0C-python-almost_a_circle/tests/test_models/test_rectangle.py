ae5a8ad5005d27bf867c7c305b02c2c2f848c63e
KVAcodes <kadiriavictor@gmail.com>
2023-03-12 22:42:17 +0100

    added new shit

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Unittesting the Rectangle class.
+"""
+
+
+import unittest
+from models import Base
+from models import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """Tests the Rectangle class.
+    """
+    @classmethod
+    def setUpClass(cls):
+        cls.rec1 = Rectangle(10, 2)
+        cls.rec2 = Rectangle(2, 10)
+        cls.rec3 = Rectangle(10, 2, 0, 0, 12)
+
+    def test_inheritance(self):
+        """tests if the rectangle instance inherits from the Base class.
+        """
+        self.assertTrue(issubclass(type(self.rec1), Base))
+
+    def test_id(self):
+        """tests if id attribute is stored with the Base class __init__
+        logic.
+        """
+        self.assertEqual(self.rec1.id, 5)
+        self.assertEqual(self.rec2.id, 6)
+        self.assertEqual(self.rec3.id, 12)
+
+    def test_setter_validation(self):
+        """tests if the setter methods and attribute instantiation are done
+        right.
+        """
+        with self.assertRaises(TypeError):
+            Rectangle(10, "2")
+        with self.assertRaises(ValueError):
+            r = Rectangle(10, 2)
+            r.width = -10
+        with self.assertRaises(TypeError):
+            r = Rectangle(10, 2)
+            r.x = {}
+        with self.assertRaises(ValueError):
+            Rectangle(10, 2, 3, -1)
+        with self.assertRaises(ValueError):
+            Rectangle(10, 2, -3, 1)
+        with self.assertRaises(TypeError):
+            Rectangle("10", 2)
