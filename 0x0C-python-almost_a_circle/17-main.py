9e78852157694a05b732a9b209d25c522fe0680b
KVAcodes <kadiriavictor@gmail.com>
2023-03-13 23:32:15 +0100

    fixed 0 error

--- 0x0C-python-almost_a_circle/17-main.py
+++ 0x0C-python-almost_a_circle/17-main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     r1 = Rectangle(3, 5, 1)
     r1_dictionary = r1.to_dictionary()
-    r2 = Rectangle.create(**r1_dictionary)
+    r2 = Rectangle.create(**{ 'width': 2, 'height': 3 })
     print(r1)
     print(r2)
     print(r1 is r2)
--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -51,8 +51,8 @@ class Base:
         """returns an instance with all attributes already set.
         """
         if cls.__name__ == "Rectangle":
-            dummy_instance = cls(1, 1, 1, 1, 1)
+            dummy_instance = cls(0, 0, 0, 0, 0)
         elif cls.__name__ == "Square":
-            dummy_instance = cls(1, 1, 1, 1)
+            dummy_instance = cls(0, 0, 0, 0)
         dummy_instance.update(**dictionary)
         return dummy_instance
