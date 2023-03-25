dccb6f6139d8085b9aa61586183f0816a348d298
KVAcodes <kadiriavictor@gmail.com>
2023-03-25 12:26:27 +0100

    added new

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,4 @@
--- list all rows of the table first_table from the database hbtn_0c_0 in MySql server.
+-- list all rows of the table first_table
+-- from the database hbtn_0c_0 in MySql server.
 
-SELECT * from first_table
+SELECT * FROM first_table
