62e925bcf86fdf2a9ab535ac2023fe9811335869
KVAcodes <kadiriavictor@gmail.com>
2023-07-22 08:01:47 +0100

    added new

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- lists all the cities of california
+SELECT c.id, c.name
+FROM cities as c
+WHERE c.state_id =
+	(SELECT s.id
+	FROM states as s
+	WHERE s.name = 'California');
+ORDER BY c.id ASC;
