e9b047f83bf4b97f54f5c23cae5ac00271aded5a
KVAcodes <kadiriavictor@gmail.com>
2023-07-22 08:26:16 +0100

    added for testing

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- lists all the cities of california
 SELECT c.id, c.name
-FROM cities as c
+FROM cities AS c
 WHERE c.state_id =
 	(SELECT s.id
-	FROM states as s
+	FROM states AS s
 	WHERE s.name = 'California')
 ORDER BY c.id ASC;
