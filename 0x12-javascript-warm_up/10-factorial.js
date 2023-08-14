bd64d16209dd2a2e762ced0fc7668cb5c7c2dfc4
KVAcodes <kadiriavictor@gmail.com>
2023-08-14 18:18:12 +0100

    fixed checker errors

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,7 +4,7 @@ const process = require('process');
 const value = parseInt(process.argv[2]);
 
 function fact (val) {
-  if (isNaN(val) || val == 1) {
+  if (isNaN(val) || val === 1) {
     return (1);
   }
   return (val * fact(val - 1));
