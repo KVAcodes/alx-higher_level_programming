52db9b1ec932d9ec64e9a91b3c5915bdb90db462
KVAcodes <kadiriavictor@gmail.com>
2023-08-21 09:32:29 +0100

    added new

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This script lists all the states from the database hbtn_0e_0_usa.
+with a name starting with N (upper N).
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE NAME LIKE \"N%\";")
+
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
