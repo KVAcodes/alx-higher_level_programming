17722d334a74a83fbd5b055115f9716662e4d331
KVAcodes <kadiriavictor@gmail.com>
2023-08-21 10:19:58 +0100

    fixed case sensitivity

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = \"{}\" "
-                "ORDER BY states.id;".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name = BINARY \"{}\" "
+                "ORDER BY states.id ASC;".format(sys.argv[4]))
 
     for row in cur.fetchall():
         print(row)
