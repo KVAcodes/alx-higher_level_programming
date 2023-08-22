857d0941207aaa18136f6570e59f4d3fa6d8efef
KVAcodes <kadiriavictor@gmail.com>
2023-08-22 09:28:25 +0100

    added task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" a script that lists all State objects from the database hbtn_0e_6_usa.
-with the letter a in it.
+"""a script that prints the State object with the name passed
+as argument from the database hbtn_0e_6_usa.
 """
 
 import sys
@@ -14,7 +14,9 @@ if __name__ == "__main__":
     engine.connect()
     Session = sessionmaker(bind=engine)
     session = Session()
-    qry = session.query(State).filter(
-          State.name.like('%a%')).order_by(State.id)
-    for row in qry:
-        print(row.id, ': ', row.name, sep='')
+    qry = session.query(State).filter(State.name == sys.argv[4]).first()
+
+    if qry:
+        print(qry.id)
+    else:
+        print("Not found")
