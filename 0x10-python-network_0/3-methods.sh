9b6dd970843d63b90dcd6169b55e2f9c3f4d8549
KVAcodes <kadiriavictor@gmail.com>
2023-09-04 00:34:48 +0100

    added new

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept.
-curl -X OPTIONS -sL "$1"
+curl -X OPTIONS -sL "$1" -i | awk '/Allow: / { sub(/Allow: /, ""); print }'
