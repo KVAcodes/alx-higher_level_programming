208bc114478742f5e1b2e9f105c7507fb3ae5230
KVAcodes <kadiriavictor@gmail.com>
2023-09-04 20:14:55 +0100

    added task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This is a script that fetches https://alx-intranet.hbtn.io/status.
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    with (
+        urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
+    ) as response:
+        content_byte = response.read()
+        formatted_output = (
+            "Body response:\n"
+            "   - type: {}\n"
+            "   - content: {}\n"
+            "   - utf8 content: {}"
+        ).format(type(content_byte), content_byte,
+                 content_byte.decode(encoding="utf-8"))
+        print(formatted_output)
