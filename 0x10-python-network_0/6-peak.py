c8a80772827c262afef5b5d1d24fd709909e92e7
KVAcodes <kadiriavictor@gmail.com>
2023-09-04 12:28:31 +0100

    fixed some errors

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,8 +7,12 @@ def find_peak(list_of_integers):
     """This function finds a peak in a list of
     unsorted integers.
     """
-    if list_of_integers:
-        list_of_integers.sort()
-    else:
+    if not list_of_integers:
         return
-    return list_of_integers[-1]
+    start, end = 0, len(list_of_integers) - 1
+    centre = (end + start) // 2 + 1
+
+    if end == 0:
+        return list_of_integers[start]
+    return max(find_peak(list_of_integers[start:centre]),
+               find_peak(list_of_integers[centre:]))
