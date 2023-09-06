07fe9b3f6e8121f2715e2cbb9c1a52e97b4809a1
KVAcodes <kadiriavictor@gmail.com>
2023-09-06 16:23:48 +0000

    added task 3 for testing

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8).manage
+urllib.error.HTTPError exceptions and print: Error code: followed
+by the HTTP status code.
+"""
+import urllib.request
+from urllib.error import HTTPError
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        with (
+            urllib.request.urlopen(sys.argv[1])
+        ) as response:
+            content_byte = response.read()
+            print(formatted_output)
+    except HTTPError as e:
+        print(f"Error code: {e.code}")
