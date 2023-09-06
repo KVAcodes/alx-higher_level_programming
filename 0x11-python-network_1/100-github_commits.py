ff2f4317938606af26e6eddf63d964bc8134763c
KVAcodes <kadiriavictor@gmail.com>
2023-09-06 20:56:25 +0000

    added

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-"""script that takes your GitHub credentials (username and password)
-and uses the GitHub API to display your id.
+"""lists the 10 most recent commits on a given GitHub repository.
 """
 import sys
 import requests
 
 
-if __name__ == '__main__':
-    base_url = "https://api.github.com/repos/"
-    owner = f"{sys.argv[1]}/"
-    repo = f"{sys.argv[2]}/"
-    url = base_url + owner + repo + "commits"
-    with (
-        requests.get(url)
-    ) as response:
-        response_json = response.json()
-        i = 0
-        while i < 10 and i < len(response_json):
-            sha = response_json[i]['sha']
-            author = response_json[i]['commit']['author']['name']
-            print(f"{sha}: {author}")
-            i += 1
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
