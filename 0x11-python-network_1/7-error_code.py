029d7327c346d24d7b671c3620935f2539502b6d
KVAcodes <kadiriavictor@gmail.com>
2023-09-06 16:59:58 +0000

    added task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays
+the body of the response.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    with (
+        requests.get(sys.argv[1])
+    ) as response:
+        if response.status_code[0] >= '4':
+            print(f"Error code: ${response.status_code}")
+        else:
+            print(response.text)
